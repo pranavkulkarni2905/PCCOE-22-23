f4dd6f65bbfb78bb3b607034b44f5a64c62cfabc
Pranav Kulkarni <pranavkulkarni1110@gmail.com>
2022-11-27 12:58:39 +0530

    added DSA practical 1

--- PBL-1/Group 1/practical1.cpp
+++ PBL-1/Group 1/practical1.cpp
@@ -1,6 +1,18 @@
 /*
 Date:22-11-2022
 Programmer:Pranav Kulkarni
+Title:Define a class to represent a bank account which includes the following 
+members as:
+Data members:
+a. Name of the depositor
+b. Account Number
+c. Withdrawal amount
+d. Balance amount in the account Member Functions:
+e. To assign initial values
+f. To deposit an amount
+g. To withdraw an amount after checking the balance
+h. To display name and balance.
+Implement the program by using features of OOP in C++
 */
 
 #include <iostream>
