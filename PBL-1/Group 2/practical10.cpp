d2942258613e32c4b6d1a78583c6a60d409f40b1
Pranav Kulkarni <pranavkulkarni1110@gmail.com>
2022-12-01 23:05:14 +0530

    NA

--- PBL-1/Group 2/practical10.cpp
+++ PBL-1/Group 2/practical10.cpp
@@ -1,81 +1,105 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+
 using namespace std;
 
-class Set{
-	int no;
-	string name[20];
-	
-	public:
-		void accept();
-		void display();
-		void union1(Set b);
+class Set
+{
+private:
+    int n;
+    string name[20];
+
+
+public:
+    void read();
+    void display();
+    void unionofsets(Set c);
+
 };
 
-void Set::accept(){
-	int i=0;
-	cout<<"\nEnter number of customers:";
-	cin>>no;
-	for(i=0;i<no;i++){
-		cout<<"\nEnter customer "<<i+1<<" Data :";
-		cout<<"\nEnter customer name :";
-		cin>>name[i];
-	}
+void Set :: read(){
+    cout << "Enter no of customers : ";
+    cin >> n;
+    cout << "Enter name of customers  : \n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> name[i];
+    }  
 }
 
-void Set::display(){
-	for(int i=0;i<no;i++){
-		cout<<name[i]<<"\t";
-	}
-	cout<<"\n";
+void Set :: display(){
+    cout << "Displaying elements of set  : "<<endl;
+    for (int i = 0; i < n; i++)
+    {
+    cout << name[i] << "\t";
+       
+    }
 }
 
-void Set::union1(Set b){
-	int len1=(sizeof(name)/sizeof(name[0]));
-	int len2=(sizeof(b.name)/sizeof(b.name[0]));
-	string temp[20];
-	for(int i=0;i<len1;i++){
-		temp[i]=name[i];
-		cout<<"Added "<<temp[i]<<"\n";
-	}
-	
-	cout<<"hiiii";
-	for(int j=0;j<len2;j++){
-		cout<<"inside 2nd for looop";
-		for(int k=0;k<len1+len2;k++){
-			int b1=len1;
-			if(temp[k]!=b.name[j]){
-				temp[b1]=b.name[j];
-				cout<<temp[b1];
-				b1++;
-				}else{
-					break;
-				}
-			
-			}
-				
-		}
-		cout<<"\n--------------";
-		for(int i=0;i<len1+len2;i++)
-		{
-			cout<<"hello\n";
-			cout<<temp[i];
-		}
-	
+void Set :: unionofsets(Set setB){
+    int n2 = setB.n;
+    Set temp;
+    int flag;
+    //cout << n2;
+    // printing elements of set a
+    for (int i = 0; i < n; i++)
+    {
+        temp.name[i]=name[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << temp.name[i] << "\t";
+    }
+    
+
+    //checking common elements in both set
+    for (int j = 0; j < n2; j++)
+    {
+    flag = 0;
+        for (int k = 0; k < n; k++)
+        {
+            if (setB.name[j]==temp.name[k])
+            {
+                flag=1;
+                break;
+                
+            } 
+        }
+        if (flag!=1)
+        {
+            cout << setB.name[j] << "\t";
+        }
+         
+    }
+    
+
+
+    
+
+
 }
+
+
 int main(){
-	
-	Set setu,seta,setb;
-	
-	cout<<"\nEnter elements in Set A ( who likes Pizza):";
-	seta.accept();
-	
-	cout<<"\nEnter elements in Set B (who likes Burger):";
-	setb.accept();
-	
-	cout<<"\nEnter elements in Set U (who likes Both or doesn't like any):";
-	setu.accept();
-	
-	seta.union1(setb);
-	
+
+    Set setA;
+    Set setB;
+    Set c;
+
+    cout << "Enter customers who likes pizza : "<<endl;
+    setA.read();
+    //setA.display();
+
+    cout << "\nEnter customers who likes Burger : "<<endl;
+    setB.read();
+    //setB.display();
+
+    cout << "\n\nSet of customers who like either pizza or burger or both : "<<endl;
+    setA.unionofsets(setB);
+
+
+
+
+
+    return 0;
 }
