460a171aa54fe023160bc2e835ae842c41bbaa7e
Pranav Kulkarni <pranavkulkarni1110@gmail.com>
2022-12-02 06:59:54 +0530

    added PBL 10 practical

--- PBL-1/Group 2/practical10.cpp
+++ PBL-1/Group 2/practical10.cpp
@@ -1,3 +1,13 @@
+/*
+Title:Set A of customers like pizza and set B of customers like a burger. Write a C + +program to store two sets 
+using an array. compute and displaya. Set of customers who like either pizza or burger or both 
+b. Set of customers who like both pizza and burger.
+c. Set of customers who like only pizza, not 
+burger.
+d. Set of customers who like only burger not 
+pizza.
+e. Number of customers who like neither pizza nor burger. 
+*/
 #include <iostream>
 
 using namespace std;
@@ -13,6 +23,9 @@ public:
     void read();
     void display();
     void unionofsets(Set c);
+    void intersection(Set c);
+    void a_minus_b(Set c);
+    void b_minus_a(Set c);
 
 };
 
@@ -71,14 +84,85 @@ void Set :: unionofsets(Set setB){
         }
          
     }
-    
-
-
-    
 
 
 }
 
+    void Set :: intersection(Set setB){
+        int n2 = setB.n;
+        int flag,count=0;
+        for (int i = 0; i < n; i++)
+        {
+            flag=0;
+            for (int j = 0; j < n2; j++)
+            {
+                if (name[i]==setB.name[j])
+                {
+                    flag=1;
+                    break;
+                }
+                
+            }
+            if (flag==1)
+            {
+                count++;
+                cout << name[i] << "\t";              
+            }
+        }
+        
+            if (count==0)
+            {
+                cout << "None"<< endl;
+            }
+
+    }
+
+    void Set ::a_minus_b(Set setB){
+        int n2 = setB.n;
+        int flag;
+
+        for (int i = 0; i < n; i++)
+        {
+            flag = 0;
+            for (int j = 0; j < n2; j++)
+            {
+                if (name[i]==setB.name[j])
+                {
+                    flag = 1;
+                    break;
+                }
+                
+            }
+            if (flag!=1)
+            {
+                cout << name[i] << "\t";
+            }        
+        }
+        
+    }
+
+    void Set :: b_minus_a(Set setB){
+        int n2 = setB.n;
+        int flag;
+
+        for (int i = 0; i < n2; i++)
+        {
+            flag = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (setB.name[i]==name[j])
+                {
+                    flag = 1;
+                    break;
+                }
+                
+            }
+            if (flag!=1)
+            {
+                cout << setB.name[i] << "\t";
+            }        
+        }       
+    }
 
 int main(){
 
@@ -96,6 +180,17 @@ int main(){
 
     cout << "\n\nSet of customers who like either pizza or burger or both : "<<endl;
     setA.unionofsets(setB);
+
+    cout << "\n\nSet of customers who like both pizza and burger : "<<endl;
+    setA.intersection(setB);
+
+    cout << "\n\nSet of customers who like only pizza, not burger : "<<endl;
+    setA.a_minus_b(setB);
+
+    cout << "\n\nSet of customers who like only burger not pizza : "<<endl;
+    setA.b_minus_a(setB);
+
+
 
 
 
