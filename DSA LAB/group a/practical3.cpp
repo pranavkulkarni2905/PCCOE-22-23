fd05b6206943b1e6b5812ff946dbcab78918beea
Pranav Kulkarni <pranavkulkarni1110@gmail.com>
2022-12-24 19:05:50 +0530

    added DSA practicals

--- DSA LAB/group a/practical3.cpp
+++ DSA LAB/group a/practical3.cpp
@@ -3,103 +3,152 @@ Author:Pranav Kulkarni (SYCOA288)
 Title:Write a C++ program to perform infix to postfix conversion using stack.
 
 */
-#include<iostream>
-#include<stack> using
-namespace std;
-bool isOperator(char c)
+#include <iostream>
+using namespace std;
+
+// # Structure
+struct stack
 {
-if(c=='+'||c=='-'||c=='*'||c=='/'||c=='^')
+    int size;
+    int top;
+    char *arr;
+};
+
+// # Peek
+int stackTop(struct stack *sp)
 {
-return true;
+    return sp->arr[sp->top];
 }
-else
+
+// # Empty
+int isEmpty(struct stack *ptr)
 {
-return false;
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
-}
-int precedence(char c)
+
+// # Full
+int isFull(struct stack *ptr)
 {
- if(c == '^') return 3;
-else if(c == '*' || c == '/')
-return 2; else if(c == '+'
-|| c == '-') return 1;
-else return -1;
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
-string InfixToPostfix(stack<char> s, string infix)
+
+// # Push
+void push(struct stack *ptr, char val)
 {
-string postfix;
-for(int i=0;i<infix.length();i++)
+    if (isFull(ptr))
+    {
+        cout << "Stack Overflow! Cannot push " << val << " to the stack " << endl;
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
+
+// # Pop
+char pop(struct stack *ptr)
 {
-if((infix[i] >= 'a' && infix[i] <= 'z')
-||(infix[i] >= 'A' && infix[i] <= 'Z'))
+    if (isEmpty(ptr))
+    {
+        cout << "Stack Underflow! Cannot pop from the stack" << endl;
+        return -1;
+    }
+    else
+    {
+        char val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+// # Precedence
+int precedence(char ch)
 {
-postfix+=infix[i];
+    if (ch == '*' || ch == '/')
+        return 3;
+    else if (ch == '+' || ch == '-')
+        return 2;
+    else
+        return 0;
 }
-else if(infix[i] == '(')
+
+// # Operator
+int isOperator(char ch)
 {
-s.push(infix[i]);
+    if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
+        return 1;
+    else
+        return 0;
 }
-else if(infix[i] == ')')
+
+// # Conversion Function
+char *InfixToPostfix(char *infix)
 {
-while((s.top()!='(') && (!s.empty()))
-{
-char temp=s.top();
-postfix+=temp;
-s.pop();
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size * sizeof(char));
+    char *postfix = (char *)malloc((strlen(infix) + 1) * sizeof(char));
+    int i = 0; // Track infix traversal
+    int j = 0; // Track postfix addition
+    while (infix[i] != '\0')
+    {
+        if (!isOperator(infix[i]))
+        {
+            postfix[j] = infix[i];
+            j++;
+            i++;
+        }
+        else
+        {
+            if (precedence(infix[i]) > precedence(stackTop(sp)))
+            {
+                push(sp, infix[i]);
+                i++;
+            }
+            else
+            {
+                postfix[j] = pop(sp);
+                j++;
+            }
+        }
+    }
+    while (!isEmpty(sp))
+    {
+        postfix[j] = pop(sp);
+        j++;
+    }
+    postfix[j] = '\0';
+    return postfix;
 }
-if(s.top()=='(')
-{
-s.pop();
-}
-}
-else if(isOperator(infix[i]))
-{
-if(s.empty())
-{
-s.push(infix[i]);
-}
-else
-{
-if(precedence(infix[i])>precedence(s.top()))
-{
-s.push(infix[i]);
-}
-else if((precedence(infix[i])==precedence(s.top()))&&(infix[i]=='^'))
-{
-s.push(infix[i]);
-}
-else
-{
-while((!s.empty())&&(
-precedence(infix[i])<=precedence(s.top())))
-{
-postfix+=s.top();
-s.pop();
-}
-s.push(infix[i]);
-}
-}
-}
-}
-while(!s.empty())
-{
-postfix+=s.top();
-s.pop();
-}
-return postfix;
-}
+
+// # Main Function
 int main()
 {
- string infix_exp, postfix_exp;
-
- cout << "\nEnter a Infix Expression:" << "\n" ;
-cin >> infix_exp;
-
- stack <char> stack;
-
-cout << "\nINFIX EXPRESSION: " << infix_exp << "\n" ;
- postfix_exp = InfixToPostfix(stack, infix_exp); cout <<
-"\n" << "\nPOSTFIX EXPRESSION: " << postfix_exp;
-
-return 0;
+    char infix[30];
+    cout << "\n\t------------------------";
+    cout << "\n\t| # Infix To Postfix # |\n";
+    cout << "\t------------------------" << endl;
+    cout << "\n\t# Enter Infix Expression: ";
+    cin >> infix;
+    // char *infix[] = "x-y/z-k*d";  // Fixed Input
+    cout << "\n\t# The Infix Expression: " << infix << endl;
+    cout << "\n\t# The Postfix Expression: " << InfixToPostfix(infix) << endl;
+    cout << endl;
+    return 0;
 }
--- DSA LAB/group b/practical 11.cpp
+++ DSA LAB/group b/practical 11.cpp
@@ -41,6 +41,20 @@ void inorder(struct node* root){
   inorder(root->right);
  }
 }
+void preorder(struct node* root){
+ if(root!=NULL){
+ 	cout<<root->data<<"-->";
+	preorder(root->left);
+	preorder(root->right);
+ }
+}
+void postorder(struct node* root){
+ if(root!=NULL){
+  postorder(root->left);
+  postorder(root->right);
+  cout<<root->data<<"-->";
+ }
+}
 int search(struct node* root ,int data){
  int flag=0;
  if(root==NULL){
@@ -62,15 +76,58 @@ int search(struct node* root ,int data){
   return flag;
  }
 }
+// Find the inorder successor
+struct node *minValueNode(struct node *node) {
+  struct node *current = node;
 
+  // Find the leftmost leaf
+  while (current && current->left != NULL)
+    current = current->left;
+
+  return current;
+}
+
+// Deleting a node
+struct node *deleteNode(struct node *root, int key) {
+  // Return if the tree is empty
+  if (root == NULL) return root;
+
+  // Find the node to be deleted
+  if (key < root->data)
+    root->left = deleteNode(root->left, key);
+  else if (key > root->data)
+    root->right = deleteNode(root->right, key);
+  else {
+    // If the node is with only one child or no child
+    if (root->left == NULL) {
+      struct node *temp = root->right;
+      delete root;
+      return temp;
+    } else if (root->right == NULL) {
+      struct node *temp = root->left;
+      delete root;
+      return temp;
+    }
+
+    // If the node has two children
+    struct node *temp = minValueNode(root->right);
+
+    // Place the inorder successor in position of the node to be deleted
+    root->data = temp->data;
+
+    // Delete the inorder successor
+    root->right = deleteNode(root->right, temp->data);
+  }
+  return root;
+}
 int main()
 {
-    int choice,ch,key,flag;
+    int choice,ch,key,flag,no;
     struct node* root=NULL;
     do{
      
       cout<<"\n----BINARY SEARCH TREE OPERATIONS----";
-      cout<<"\n1.Insert Node\n2.Display All Nodes(Inorder Traversal\n3.Search Node\n4.Delete Node\n5.Exit";
+      cout<<"\n1.Insert Node\n2.Display All Nodes\n3.Search Node\n4.Delete Node\n5.Exit";
       cout<<"\nEnter Your Choice :";
       cin>>ch;
       switch(ch){
@@ -82,6 +139,10 @@ int main()
        case 2:
         cout<<"\nInorder Traversal...";
         inorder(root);
+        cout<<"\nPreorder Traversal...";
+        preorder(root);
+        cout<<"\nPostorder Traversal...";
+        postorder(root);
         break;
        case 3:
         cout<<"\nEnter Node Value to search:";
@@ -94,7 +155,9 @@ int main()
         }
         break;
        case 4:
-       
+       	cout<<"\nEnter Node Value to delete the node:";
+       	cin>>no;
+       	deleteNode(root,no);
         break;
        case 5:
        
